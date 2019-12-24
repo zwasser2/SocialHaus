807ad9404035ed2d127805bb53f7a0c2ca1d7f99
Zachary Wasserman <zwasser2@illinois.edu>
2019-12-23 20:43:59 -0800

    card working

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql')
+var url = require('url');
 
 // The database is not used for anything outside of my example and is shut down.
 var mysqlConnection = mysql.createConnection({
@@ -23,30 +24,26 @@ router.get('/', function(req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-    mysqlConnection.query('SELECT * FROM extension', (err, rows, fields) => {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    if (typeof query.id !== 'undefined') {
+        req.body = {
+            ID: query.id,
+            date: new Date()
+        }
+        let emp = req.body
+        mysqlConnection.query('INSERT INTO CheckIn1 SET ?', emp, function (err, res, fields) {
+            if(err)
+                console.log('ERROR: ' + err)
+        });
+    }
+    mysqlConnection.query('SELECT * FROM CheckIn1', (err, rows, fields) => {
         if (!err) {
-
             res.render('index', { title: JSON.stringify(rows) });
-
-
         } else {
-            console.log(err + 'ahhh')
+            console.log(err)
         }
     })
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body)
-  req.body = {
-    ID: 'Zacharyswasserman@gmail.com',
-      LastName: 'Wasserman1',
-      FirstName: 'Zachar1y',
-  }
-    let emp = req.body
-    res.render('index', { title: 'nope' });
-    mysqlConnection.query('INSERT INTO Members SET ?', emp, function (err, res, fields) {
-        if(err)
-            console.log('ERROR: ' + err)
-    });
-})
 module.exports = router;
